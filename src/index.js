37ac5bbd4e7d5dbca41fa343998b7c9337e294a3
ha thanh tam <thanhtam@has-MacBook-Pro.local>
2020-10-14 17:29:20 +0800

    Sample JSON Mock server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// 1. Create json server
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+
+// 2. Set default router and middleware
+const router = jsonServer.router({});
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// 3. Handling request and response
+const PORT = 3002; // Port value is changeable
+const THROTTLE = 1500; // Time delay for each request
+
+router.render = (req, res) => {
+	const path = req.originalUrl;
+	try {
+		const mock = require('./mockdata' + path + '/response.json');
+		setTimeout(() => {
+			res.status(200).jsonp(mock);
+		}, Math.random() * THROTTLE);
+	} catch (error) {
+		res.status(200).jsonp(`API ${path} is not mocked (existed)`);
+	}
+};
+
+// 4. Start server
+server.listen(PORT, () => {
+	console.log('JSON Server is running');
+});
+
+// 5. Sample testing URL
+// http://localhost:3002/bo/user/getUserById
